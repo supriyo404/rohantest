84b9b15f5d8de18cc0861dc7c1c83a05b575f52b
Supriyo <supriya.mondal@outlook.in>
2023-09-17 11:36:21 +0530

    message

--- src/main/java/com/example/mysite/MysiteApplication.java
+++ src/main/java/com/example/mysite/MysiteApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MysiteApplication {
-	
 
+//this is first project
 	public static void main(String[] args) {
 		SpringApplication.run(MysiteApplication.class, args);
 	}
